751e097cf0019212c60876a15b4826afc2498ba6
YenyVillalba <yeny_0192@hotmail.com>
2024-08-27 17:26:23 -0500

    componentes react
    reutilizar componentes

---
+++ src/MiNombre.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MiNombre (){
+    return (
+        <div>
+            <h2>
+                 Yeny Villalba
+            </h2>
+        </div>
+    );
+}
+export default MiNombre
---
+++ src/WelcomeMessage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function WelcomeMessage(){
+    return (
+        <div>
+            <h1>
+                ¡Bienvenidos a mi App de React!
+            </h1>
+        </div>
+    );
+}
+export default WelcomeMessage
