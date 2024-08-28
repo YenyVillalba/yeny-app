eb5ab4242f7c290290794fe772a678eedf3d40aa
YenyVillalba <yeny_0192@hotmail.com>
2024-08-27 19:28:32 -0500

    Update WelcomeMessage.js

--- src/WelcomeMessage.js
+++ src/WelcomeMessage.js
@@ -4,7 +4,7 @@ function WelcomeMessage(){
     return (
         <div>
             <h3>
-                ¡Bienvenidos a mi App de React!
+                ¡Bienvenidos a mi App de React! de mi aplicacion
             </h3>
         </div>
     );
